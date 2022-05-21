77dcda1dac423ccdcd28363b251eea09895d07fe
Patricia <99836499+siwikPatricia@users.noreply.github.com>
2022-05-21 10:46:02 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,7 @@
+"use strict";
+function openMenu() {
+  document.getElementById("navboxMobile").style.display = "block";
+}
+function closeMenu() {
+  document.getElementById("navboxMobile").style.display = "none";
+}
